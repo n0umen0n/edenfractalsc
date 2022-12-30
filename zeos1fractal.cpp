a2c48e7444aef709b5aadfcb2ad4cf562d2d19c9
n0umen0n <46645013+n0umen0n@users.noreply.github.com>
2022-12-30 21:51:28 +0200

    proposals/token tables

--- zeos1fractal.cpp
+++ zeos1fractal.cpp
@@ -1,226 +1,308 @@
 #include "zeos1fractal.hpp"
 
-zeos1fractal::zeos1fractal(
-    name self,
-    name code, 
-    datastream<const char *> ds
-) :
-    contract(self, code, ds),
-    _global(_self, _self.value)
-{
-}
+zeos1fractal::zeos1fractal(name self, name code, datastream<const char *> ds)
+    : contract(self, code, ds), _global(_self, _self.value) {}
 
-void zeos1fractal::cleartables()
-{
-    require_auth(_self);
-    members_t members(_self, _self.value);
-    intros_t introductions(_self, _self.value);
-    modranks_t modranks(_self, _self.value);
-    introranks_t introranks(_self, _self.value);
-    propranks_t propranks(_self, _self.value);
-    joins_t joins(_self, _self.value);
-    for(auto it = members.begin(); it != members.end(); ++it) members.erase(it);
-    for(auto it = introductions.begin(); it != introductions.end(); ++it) introductions.erase(it);
-    for(auto it = modranks.begin(); it != modranks.end(); ++it) modranks.erase(it);
-    for(auto it = introranks.begin(); it != introranks.end(); ++it) introranks.erase(it);
-    for(auto it = propranks.begin(); it != propranks.end(); ++it) propranks.erase(it);
-    for(auto it = joins.begin(); it != joins.end(); ++it) joins.erase(it);
-    if(_global.exists()) _global.remove();
-}
+// also includes picking yes or no.
+void zeos1fractal::voteprop(const name &user, const vector &<uint64_t> ids,
+                            const vector &<uint8_t> option) {
+  require_auth(user);
 
-void zeos1fractal::init()
-{
-    require_auth(_self);
-    cleartables();
+  symbol sym = symbol("REZPECT", 4);
 
-    _global.set({
-        CS_IDLE,
-        0,
-        0,
-        vector<name>(),
+  accounts baltable(_self, user.value);
 
-        0,
-        vector<name>(),
-        vector<uint64_t>(),
+  const auto &baliter =
+      baltable.get(sym.code().raw(), "You have no REZPECT tokens brother.");
 
-        map<name, uint64_t>({
-            {"moderator"_n, 10},    // respect required to be able to become a moderator
-            {"delegate"_n, 100},    // respect required to be able to become a delegate
-            {"modvote"_n, 200},     // respect required to be able to vote for moderators
-            {"propvote"_n, 300}     // respect required to be able to vote for proposals
-            // TODO ...
-        }),
-        3600,                       // 30 min
-        5400,                       // 45 min
-        5400,                       // 45 min
-        1200                        // 10 min
-    }, _self);
-}
+  usrpropvote_t usrvote(_self, _self.value);
+  const auto &voteiter = usrvote.find(user.value);
 
-void zeos1fractal::signup(const name& user)
-{
-    require_auth(user);
-    members_t members(_self, _self.value);
-    check(members.find(user.value) == members.end(), "user is already signed up!");
+  // meaning user has voted before
+  if (voteiter != usrvote.end())
 
-    members.emplace(user, [&](auto& row){
-        row.user = user;
-        row.links = map<string, string>();
-        row.zeos_earned = 0;
-        row.respect_earned = 0;
-        row.accept_moderator = false;
-        row.accept_delegate = false;
-        row.has_been_delegate = false;
-        row.is_banned = false;
-    });
+  {
 
-    // TODO: create balance for Respect token to allocate RAM at cost of user for later respect token issuance
-}
+    for (size_t i = 0; i < voteiter.ids.size(); i++) {
 
-void zeos1fractal::join(const name& user)
-{
-    require_auth(user);
-    members_t members(_self, _self.value);
-    check(members.find(user.value) != members.end(), "user is not signed up yet!");
-    joins_t joins(_self, _self.value);
-    check(joins.find(user.value) == joins.end(), "user has joined already!");
-    check(_global.exists(), "'global' not initialized! call 'init' first");
-    auto g = _global.get();
-    check(g.next_event_block_height > 0, "no event upcoming yet!");
-    check(static_cast<uint64_t>(current_block_number()) >= g.next_event_block_height - g.early_join_duration, "too early to join the event!");
+      // go to proposals to proposals table
+      // proposals_t proptab(_self, _self.value);
+      const auto &proprow = proptab.find(voteiter.ids[i]);
 
-    joins.emplace(user, [&](auto& row){
-        row.user = user;
-    });
-}
+      check(proprow == proptab.end(), "Proposal with such ID does not exist.");
 
-void zeos1fractal::addlink(
-    const name& user,
-    const string& key,
-    const string& value
-)
-{
-    require_auth(user);
-    members_t members(_self, _self.value);
-    auto u = members.find(user.value);
-    check(u != members.end(), "user is not signed up yet!");
+      // uint64_t adjustedbal = baliter.balance.amount / (i + 1);
 
-    // check if the key/value pair already exists for (some other) user
-    for(auto it = members.begin(); it != members.end(); ++it)
-    {
-        auto entry = it->links.find(key);
-        if(entry != it->links.end())
-        {
-            check(entry->second != value, "key/value pair already exists!");
-        }
+      proptab.modify(proprow, user, [&](auto &contract) {
+        contract.totaltokens -= adjustedbal;
+      });
+
+      if (voteiter.option[i] == 0)
+
+      {
+
+        proptab.modify(proprow, user, [&](auto &contract) {
+          contract.votedforoption[0] -= baliter.balance.amount;
+        });
+      }
+
+      if (voteiter.option[i] == 1)
+
+      {
+
+        proptab.modify(proprow, user, [&](auto &contract) {
+          contract.votedforoption[1] -= baliter.balance.amount;
+        });
+      }
     }
+  }
 
-    members.modify(u, user, [&](auto& row){
-        row.links[key] = value;
+  if (voteiter != usrvote.end())
+
+  {
+    usrvote.erase(voteiter);
+  }
+
+  usrvote.emplace(_self, [&](auto &contract) {
+    contract.user = user;
+    contract.ids = ids;
+    contract.option = option;
+  });
+
+  for (size_t i = 0; i < ids.size(); i++) {
+
+    // proposals_t proptab(_self, _self.value);
+    const auto &proprow = proptab.find(ids[i]);
+
+    check(proprow == proptab.end(),
+          "Proposal with such ID does not exist (1).");
+
+    proptab.modify(proprow, user, [&](auto &contract) {
+      contract.totaltokens -= adjustedbal;
     });
-}
+    // 0=no
+    if (option[i] == 0)
 
-void zeos1fractal::setacceptmod(
-    const name& user,
-    const bool& value
-)
-{
-    require_auth(user);
-    members_t members(_self, _self.value);
-    auto u = members.find(user.value);
-    check(u != members.end(), "user is not signed up yet!");
-
-    members.modify(u, user, [&](auto& row){
-        row.accept_moderator = value;
-    });
-}
-
-void zeos1fractal::setacceptdel(
-    const name& user,
-    const bool& value
-)
-{
-    require_auth(user);
-    members_t members(_self, _self.value);
-    auto u = members.find(user.value);
-    check(u != members.end(), "user is not signed up yet!");
-
-    members.modify(u, user, [&](auto& row){
-        row.accept_delegate = value;
-    });
-}
-
-void zeos1fractal::setintro(
-    const name& user,
-    const uint64_t& num_blocks
-)
-{
-    require_auth(user);
-    members_t members(_self, _self.value);
-    check(members.find(user.value) != members.end(), "user is not signed up yet!");
-
-    // TODO
-}
-
-void zeos1fractal::setevent(const uint64_t& block_height)
-{
-    require_auth(_self);
-    check(_global.exists(), "'global' not initialized! call 'init' first");
-    auto g = _global.get();
-    
-    if(block_height == 0)
     {
-        g.next_event_block_height = 0;
-        _global.set(g, _self);
+
+      proptab.modify(proprow, user, [&](auto &contract) {
+        contract.votedforoption[0] -= baliter.balance.amount;
+      });
     }
-    else
+    // 1=yes
+    if (option[i] == 1)
+
     {
-        check(block_height > static_cast<uint64_t>(current_block_number()), "block_height too low: event must start in the future!");
-        g.next_event_block_height = block_height;
-        _global.set(g, _self);
+
+      proptab.modify(proprow, user, [&](auto &contract) {
+        contract.votedforoption[1] -= baliter.balance.amount;
+      });
     }
+  }
+
+  // get users balance
+  // check if has anything if not error no balance
+
+  // If exists users vote table. Scope self, key by name. Then go to proposals
+  // table loop number of times the vector ids size. Substract from option and
+  // from total voted for proposal. then delete the vote table then emplace
+  // the table and add new votes.
 }
 
-void zeos1fractal::changestate()
-{
-    // anyone can execute this action
-    check(_global.exists(), "'global' not initialized! call 'init' first");
-    auto g = _global.get();
-    uint64_t cur_bh = static_cast<uint64_t>(current_block_number());
+void zeos1fractal::voteintro(const name &user, const vector<name &> ids) {
+  // will see if separate actio needed
+}
 
-    switch(g.state)
-    {
-        case CS_IDLE:
-        {
-            check(cur_bh > g.next_event_block_height, "too early to move into INTRODUCTION state!");
-            g.state = CS_INTRODUCTION;
-            _global.set(g, _self);
-        }
-        break;
+void zeos1fractal::cleartables() {
+  require_auth(_self);
+  members_t members(_self, _self.value);
+  intros_t introductions(_self, _self.value);
+  modranks_t modranks(_self, _self.value);
+  introranks_t introranks(_self, _self.value);
+  propranks_t propranks(_self, _self.value);
+  joins_t joins(_self, _self.value);
+  for (auto it = members.begin(); it != members.end(); ++it)
+    members.erase(it);
+  for (auto it = introductions.begin(); it != introductions.end(); ++it)
+    introductions.erase(it);
+  for (auto it = modranks.begin(); it != modranks.end(); ++it)
+    modranks.erase(it);
+  for (auto it = introranks.begin(); it != introranks.end(); ++it)
+    introranks.erase(it);
+  for (auto it = propranks.begin(); it != propranks.end(); ++it)
+    propranks.erase(it);
+  for (auto it = joins.begin(); it != joins.end(); ++it)
+    joins.erase(it);
+  if (_global.exists())
+    _global.remove();
+}
 
-        case CS_INTRODUCTION:
-        {
-            check(cur_bh > g.next_event_block_height + g.introduction_duration, "too early to move into BREAKOUT_ROOMS state!");
-            g.state = CS_BREAKOUT_ROOMS;
-            _global.set(g, _self);
-        }
-        break;
+void zeos1fractal::init() {
+  require_auth(_self);
+  cleartables();
 
-        case CS_BREAKOUT_ROOMS:
-        {
-            check(cur_bh > g.next_event_block_height + g.introduction_duration + g.breakout_room_duration, "too early to move into COUNCIL_MEETING state!");
-            g.state = CS_COUNCIL_MEETING;
-            _global.set(g, _self);
-        }
-        break;
+  _global.set(
+      {
+          CS_IDLE, 0, 0, vector<name>(),
 
-        case CS_COUNCIL_MEETING:
-        {
-            check(cur_bh > g.next_event_block_height + g.introduction_duration + g.breakout_room_duration + g.council_meeting_duration, "too early to move into IDLE state!");
-            check(g.next_event_block_height == 0 || g.next_event_block_height > cur_bh, "next event is not yet set by council! call 'setevent' to creat a new event.");
-            g.state = CS_IDLE;
-            _global.set(g, _self);
-        }
-        break;
+          0, vector<name>(), vector<uint64_t>(),
+
+          map<name, uint64_t>({
+              {"moderator"_n,
+               10}, // respect required to be able to become a moderator
+              {"delegate"_n,
+               100}, // respect required to be able to become a delegate
+              {"modvote"_n,
+               200}, // respect required to be able to vote for moderators
+              {"propvote"_n,
+               300} // respect required to be able to vote for proposals
+                    // TODO ...
+          }),
+          3600, // 30 min
+          5400, // 45 min
+          5400, // 45 min
+          1200  // 10 min
+      },
+      _self);
+}
+
+void zeos1fractal::signup(const name &user) {
+  require_auth(user);
+  members_t members(_self, _self.value);
+  check(members.find(user.value) == members.end(),
+        "user is already signed up!");
+
+  members.emplace(user, [&](auto &row) {
+    row.user = user;
+    row.links = map<string, string>();
+    row.zeos_earned = 0;
+    row.respect_earned = 0;
+    row.accept_moderator = false;
+    row.accept_delegate = false;
+    row.has_been_delegate = false;
+    row.is_banned = false;
+  });
+
+  // TODO: create balance for Respect token to allocate RAM at cost of user
+  // for later respect token issuance
+}
+
+void zeos1fractal::join(const name &user) {
+  require_auth(user);
+  members_t members(_self, _self.value);
+  check(members.find(user.value) != members.end(),
+        "user is not signed up yet!");
+  joins_t joins(_self, _self.value);
+  check(joins.find(user.value) == joins.end(), "user has joined already!");
+  check(_global.exists(), "'global' not initialized! call 'init' first");
+  auto g = _global.get();
+  check(g.next_event_block_height > 0, "no event upcoming yet!");
+  check(static_cast<uint64_t>(current_block_number()) >=
+            g.next_event_block_height - g.early_join_duration,
+        "too early to join the event!");
+
+  joins.emplace(user, [&](auto &row) { row.user = user; });
+}
+
+void zeos1fractal::addlink(const name &user, const string &key,
+                           const string &value) {
+  require_auth(user);
+  members_t members(_self, _self.value);
+  auto u = members.find(user.value);
+  check(u != members.end(), "user is not signed up yet!");
+
+  // check if the key/value pair already exists for (some other) user
+  for (auto it = members.begin(); it != members.end(); ++it) {
+    auto entry = it->links.find(key);
+    if (entry != it->links.end()) {
+      check(entry->second != value, "key/value pair already exists!");
     }
+  }
+
+  members.modify(u, user, [&](auto &row) { row.links[key] = value; });
+}
+
+void zeos1fractal::setacceptmod(const name &user, const bool &value) {
+  require_auth(user);
+  members_t members(_self, _self.value);
+  auto u = members.find(user.value);
+  check(u != members.end(), "user is not signed up yet!");
+
+  members.modify(u, user, [&](auto &row) { row.accept_moderator = value; });
+}
+
+void zeos1fractal::setacceptdel(const name &user, const bool &value) {
+  require_auth(user);
+  members_t members(_self, _self.value);
+  auto u = members.find(user.value);
+  check(u != members.end(), "user is not signed up yet!");
+
+  members.modify(u, user, [&](auto &row) { row.accept_delegate = value; });
+}
+
+void zeos1fractal::setintro(const name &user, const uint64_t &num_blocks) {
+  require_auth(user);
+  members_t members(_self, _self.value);
+  check(members.find(user.value) != members.end(),
+        "user is not signed up yet!");
+
+  // TODO
+}
+
+void zeos1fractal::setevent(const uint64_t &block_height) {
+  require_auth(_self);
+  check(_global.exists(), "'global' not initialized! call 'init' first");
+  auto g = _global.get();
+
+  if (block_height == 0) {
+    g.next_event_block_height = 0;
+    _global.set(g, _self);
+  } else {
+    check(block_height > static_cast<uint64_t>(current_block_number()),
+          "block_height too low: event must start in the future!");
+    g.next_event_block_height = block_height;
+    _global.set(g, _self);
+  }
+}
+
+void zeos1fractal::changestate() {
+  // anyone can execute this action
+  check(_global.exists(), "'global' not initialized! call 'init' first");
+  auto g = _global.get();
+  uint64_t cur_bh = static_cast<uint64_t>(current_block_number());
+
+  switch (g.state) {
+  case CS_IDLE: {
+    check(cur_bh > g.next_event_block_height,
+          "too early to move into INTRODUCTION state!");
+    g.state = CS_INTRODUCTION;
+    _global.set(g, _self);
+  } break;
+
+  case CS_INTRODUCTION: {
+    check(cur_bh > g.next_event_block_height + g.introduction_duration,
+          "too early to move into BREAKOUT_ROOMS state!");
+    g.state = CS_BREAKOUT_ROOMS;
+    _global.set(g, _self);
+  } break;
+
+  case CS_BREAKOUT_ROOMS: {
+    check(cur_bh > g.next_event_block_height + g.introduction_duration +
+                       g.breakout_room_duration,
+          "too early to move into COUNCIL_MEETING state!");
+    g.state = CS_COUNCIL_MEETING;
+    _global.set(g, _self);
+  } break;
+
+  case CS_COUNCIL_MEETING: {
+    check(cur_bh > g.next_event_block_height + g.introduction_duration +
+                       g.breakout_room_duration + g.council_meeting_duration,
+          "too early to move into IDLE state!");
+    check(g.next_event_block_height == 0 || g.next_event_block_height > cur_bh,
+          "next event is not yet set by council! call 'setevent' to creat a "
+          "new event.");
+    g.state = CS_IDLE;
+    _global.set(g, _self);
+  } break;
+  }
 }
--- zeos1fractal.hpp
+++ zeos1fractal.hpp
@@ -1,176 +1,182 @@
 #pragma once
 
-#include <map>
-#include <vector>
 #include <eosio/eosio.hpp>
 #include <eosio/singleton.hpp>
+#include <map>
+#include <vector>
 
 using namespace std;
 using namespace eosio;
 
 // proposal status
-#define PS_PROPOSED         0
-#define PS_UNDER_REVIEW     1
-#define PS_APPROVED         2
-#define PS_DENIED           3
+#define PS_PROPOSED 0
+#define PS_UNDER_REVIEW 1
+#define PS_APPROVED 2
+#define PS_DENIED 3
 
 // contract states
-#define CS_IDLE             0
-#define CS_INTRODUCTION     1
-#define CS_BREAKOUT_ROOMS   2
-#define CS_COUNCIL_MEETING  3
+#define CS_IDLE 0
+#define CS_INTRODUCTION 1
+#define CS_BREAKOUT_ROOMS 2
+#define CS_COUNCIL_MEETING 3
 
-CONTRACT zeos1fractal : contract
-{
-    public:
+CONTRACT zeos1fractal : contract {
+public:
+  TABLE userspropvote {
+    name user;
+    vector<uint64_t> ids;
+    vector<uint8_t> option;
 
-    TABLE member
-    {
-        name user;                  // EOS account name
-        map<string, string> links;  // for instance: twitter.com => @mschoenebeck1 or SSH => ssh-rsa AAAAB3NzaC1yc2E...
-        uint64_t zeos_earned;       // total amount of ZEOS received
-        uint64_t respect_earned;    // total amount of ZEOS Respect received
-        bool accept_moderator;      // accept moderator role?
-        bool accept_delegate;       // accept delegate vote?
-        bool has_been_delegate;     // has ever been delegate?
-        bool is_banned;             // is user banned from fractal?
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"usrspropvote"_n, usrspropvote> usrpropvote_t;
 
-        uint64_t primary_key() const { return user.value; }
-    };
-    typedef eosio::multi_index<"members"_n, member> members_t;
+  TABLE currency_stats {
+    asset supply;
+    asset max_supply;
+    name issuer;
 
-    // resets with every weekly event
-    TABLE introduction
-    {
-        name user;
-        uint64_t num_blocks; // duration of the introduction
+    uint64_t primary_key() const { return supply.symbol.code().raw(); }
+  };
 
-        uint64_t primary_key() const { return user.value; }
-    };
-    typedef eosio::multi_index<"intros"_n, introduction> intros_t;
+  typedef eosio::multi_index<name("stat"), currency_stats> stats;
 
-    // VRAM - ever expanding (can only add/modify items)
-    TABLE proposal
-    {
-        uint64_t id;
-        name user;
-        string caption;
-        string description;
-        string ipfs;
-        uint64_t status;
+  TABLE account {
+    asset balance;
 
-        uint64_t primary_key() const { return id; }
-    };
-    // TODO: change to VRAM
-    typedef eosio::multi_index<"proposals"_n, proposal> proposals_t;
+    uint64_t primary_key() const { return balance.symbol.code().raw(); }
+  };
 
-    // ongoing vote (like EOS BP voting)
-    TABLE moderator_ranking
-    {
-        name user;
-        vector<name> ranking;
+  typedef eosio::multi_index<name("accounts"), account> accounts;
 
-        uint64_t primary_key() const { return user.value; }
-    };
-    typedef eosio::multi_index<"modranks"_n, moderator_ranking> modranks_t;
+  TABLE member {
+    name user;                 // EOS account name
+    map<string, string> links; // for instance: twitter.com => @mschoenebeck1 or
+                               // SSH => ssh-rsa AAAAB3NzaC1yc2E...
+    uint64_t zeos_earned;      // total amount of ZEOS received
+    uint64_t respect_earned;   // total amount of ZEOS Respect received
+    bool accept_moderator;     // accept moderator role?
+    bool accept_delegate;      // accept delegate vote?
+    bool has_been_delegate;    // has ever been delegate?
+    bool is_banned;            // is user banned from fractal?
 
-    // resets with every event
-    TABLE introduction_ranking
-    {
-        name user;
-        vector<name> ranking;
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"members"_n, member> members_t;
 
-        uint64_t primary_key() const { return user.value; }
-    };
-    typedef eosio::multi_index<"introranks"_n, introduction_ranking> introranks_t;
+  // resets with every weekly event
+  TABLE introduction {
+    name user;
+    uint64_t num_blocks; // duration of the introduction
 
-    // resets with every event
-    TABLE proposal_ranking
-    {
-        name user;
-        vector<uint64_t> ranking;
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"intros"_n, introduction> intros_t;
 
-        uint64_t primary_key() const { return user.value; }
-    };
-    typedef eosio::multi_index<"propranks"_n, proposal_ranking> propranks_t;
+  // VRAM - ever expanding (can only add/modify items)
+  TABLE proposal {
+    uint64_t id;
+    name user;
+    string question;
+    vector<uint64_t> votedforoption;
+    vector<string> answers;
+    uint64_t totaltokens;
+    string description;
+    string ipfs;
+    uint64_t status;
 
-    // resets with every event
-    TABLE joined
-    {
-        name user;
+    uint64_t primary_key() const { return id; }
+  };
+  // TODO: change to VRAM
+  typedef eosio::multi_index<"proposals"_n, proposal> proposals_t;
 
-        uint64_t primary_key() const { return user.value; }
-    };
-    typedef eosio::multi_index<"joins"_n, joined> joins_t;
+  // ongoing vote (like EOS BP voting)
+  TABLE moderator_ranking {
+    name user;
+    vector<name> ranking;
 
-    // Singleton - Global stats
-    TABLE global
-    {
-        uint64_t state;
-        uint64_t proposal_count;
-        uint64_t event_count;
-        vector<name> moderator_ranking;
-        
-        // Event information
-        // Resets every time when a new event is created by 'admin'
-        uint64_t next_event_block_height;
-        vector<name> introduction_ranking;
-        vector<uint64_t> proposal_ranking;
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"modranks"_n, moderator_ranking> modranks_t;
 
-        // Settings/Configuration
-        map<name, uint64_t> respect_level;
-        uint64_t introduction_duration;
-        uint64_t breakout_room_duration;
-        uint64_t council_meeting_duration;
-        uint64_t early_join_duration;
-    };
-    eosio::singleton<"global"_n, global> _global;
+  // resets with every event
+  TABLE introduction_ranking {
+    name user;
+    vector<name> ranking;
 
-    zeos1fractal(
-        name self,
-        name code, 
-        datastream<const char *> ds
-    );
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"introranks"_n, introduction_ranking> introranks_t;
 
-    /// Clears all tables of the smart contract (useful if table schema needs update)
-    ACTION cleartables();
+  // resets with every event
+  TABLE proposal_ranking {
+    name user;
+    vector<uint64_t> ranking;
 
-    /// Initializes the contract and resets all tables
-    ACTION init();
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"propranks"_n, proposal_ranking> propranks_t;
 
-    /// Creates a new member account for this fractal (allocates RAM)
-    ACTION signup(const name& user);
+  // resets with every event
+  TABLE joined {
+    name user;
 
-    /// Joins the current event
-    ACTION join(const name& user);
-    
-    /// Adds link to (social media) profile on other platform. Remains forever in chain state.
-    ACTION addlink(
-        const name& user,
-        const string& key,
-        const string& value
-    );
+    uint64_t primary_key() const { return user.value; }
+  };
+  typedef eosio::multi_index<"joins"_n, joined> joins_t;
 
-    /// Set to 'true' to accept moderator role
-    ACTION setacceptmod(
-        const name& user,
-        const bool& value
-    );
+  // Singleton - Global stats
+  TABLE global {
+    uint64_t state;
+    uint64_t proposal_count;
+    uint64_t event_count;
+    vector<name> moderator_ranking;
 
-    // Set to 'true' to accept delegate vote
-    ACTION setacceptdel(
-        const name& user,
-        const bool& value
-    );
+    // Event information
+    // Resets every time when a new event is created by 'admin'
+    uint64_t next_event_block_height;
+    vector<name> introduction_ranking;
+    vector<uint64_t> proposal_ranking;
 
-    ACTION setintro(
-        const name& user,
-        const uint64_t& seconds
-    );
+    // Settings/Configuration
+    map<name, uint64_t> respect_level;
+    uint64_t introduction_duration;
+    uint64_t breakout_room_duration;
+    uint64_t council_meeting_duration;
+    uint64_t early_join_duration;
+  };
+  eosio::singleton<"global"_n, global> _global;
 
-    // TODO: add/update proposal actions
+  zeos1fractal(name self, name code, datastream<const char *> ds);
 
-    ACTION setevent(const uint64_t& block_height);
+  /// Clears all tables of the smart contract (useful if table schema needs
+  /// update)
+  ACTION cleartables();
 
-    ACTION changestate();
+  /// Initializes the contract and resets all tables
+  ACTION init();
+
+  /// Creates a new member account for this fractal (allocates RAM)
+  ACTION signup(const name &user);
+
+  /// Joins the current event
+  ACTION join(const name &user);
+
+  /// Adds link to (social media) profile on other platform. Remains forever in
+  /// chain state.
+  ACTION addlink(const name &user, const string &key, const string &value);
+
+  /// Set to 'true' to accept moderator role
+  ACTION setacceptmod(const name &user, const bool &value);
+
+  // Set to 'true' to accept delegate vote
+  ACTION setacceptdel(const name &user, const bool &value);
+
+  ACTION setintro(const name &user, const uint64_t &seconds);
+
+  // TODO: add/update proposal actions
+
+  ACTION setevent(const uint64_t &block_height);
+
+  ACTION changestate();
 };
